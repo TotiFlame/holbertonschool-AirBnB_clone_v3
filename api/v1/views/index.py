68ac3ea2e0bbdb9b53011e5ddf89cfbd3484ce41
vicomarquez <vicomarquezmuniz@gmail.com>
2022-12-07 11:44:53 -0300

    fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,17 +7,19 @@ from api.v1.views import app_views
 from models import storage
 from models.engine.db_storage import classes
 
+
 @app_views.route('/status', strict_slashes=False)
 def jsmessage():
     return(jsonify({"status": "ok"}))
 
+
 @app_views.route('/stats', strict_slashes=False)
 def num_obj():
     return jsonify({
-        "amenities": storage.count(classes["Amenity"]), 
-        "cities": storage.count(classes["City"]), 
-        "places": storage.count(classes["Place"]), 
-        "reviews": storage.count(classes["Review"]), 
-        "states": storage.count(classes["State"]), 
+        "amenities": storage.count(classes["Amenity"]),
+        "cities": storage.count(classes["City"]),
+        "places": storage.count(classes["Place"]),
+        "reviews": storage.count(classes["Review"]),
+        "states": storage.count(classes["State"]),
         "users": storage.count(classes["User"])
         })
