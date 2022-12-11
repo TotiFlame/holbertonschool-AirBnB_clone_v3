e06d3e657770da65db84c70ba2c3e09f91913ac3
TotiFlame <santifer4321@gmail.com>
2022-12-11 22:36:26 +0000

    Task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -23,3 +23,26 @@ def show_all_cities(state_id):
     for city in state.cities:
         cities_list.append(city.to_dict())
     return jsonify(cities_list)
+
+@app_views.route("/cities/<city_id>", methods=['GET'], strict_slashes=False)
+def show_city(city_id):
+    """
+    It gets a city from the database, and if it
+    doesn't exist, it returns a 404 error
+    """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+@app_views.route("/cities/<city_id>", methods=['DELETE'], strict_slashes=False)
+def del_city(city_id):
+    """
+    It deletes a city from the database
+    """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
