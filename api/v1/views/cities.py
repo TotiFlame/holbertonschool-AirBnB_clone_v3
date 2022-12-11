7f726f532e6df005da04e6aa5e28f843c2c1493e
TotiFlame <santifer4321@gmail.com>
2022-12-11 21:00:10 +0000

    Task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -15,12 +15,14 @@ def show_all_cities(state_id):
     representation of all the cities in a state
     """
     cities_list = []
-    cities =  storage.all("City").values()
-    states = storage.all("State").values()
-    for state in states:
-        if state.id == state_id:
-            for city in cities:
-                if city.state_id == state_id:
-                    cities_list.append(city.to_dict())
+    cities = storage.all("City").values()
+    
+    for states in storage.all("State").values():
+        if states.id == state_id:
+            state = storage.all()["State" + '.' + state_id]
+            if state.cities:
+                for city in cities:
+                    if city.state_id == state_id:
+                        cities_list.append(city.to_dict())
             return jsonify(cities_list)
     abort(404)
